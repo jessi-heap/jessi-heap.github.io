8e6f6596c86b77c3de311629b76dc5b9b736fc5c
jessi-heap <jessi.style@heapanalytics.com>
2024-08-01 16:25:47 +0100

    test

--- v5/config/heap_config.js
+++ v5/config/heap_config.js
@@ -1,1 +1,51 @@
-!function(){var SERVER_SIDE_CONFIG={};function a(e){var n;return-1===e.indexOf('core')&&e.push('core'),e.sort(function(e,n){return e<n?-1:n<e?1:0}).join('-')}function i(){var e=a(SERVER_SIDE_CONFIG.sdk.plugins||[]),n=heap&&heap.clientConfig&&heap.clientConfig.sdk||{},i=n.domain||SERVER_SIDE_CONFIG.sdk.domain,r=n.version||SERVER_SIDE_CONFIG.sdk.version,t;return i+'/v5/'+(n.version||!SERVER_SIDE_CONFIG.sdk.isExperimentalVersion?'heapjs-static':'heapjs-experimental')+'/'+r+'/'+e+'/heap.js'}function e(){var e=i(),n=document.createElement('script');n.type='text/javascript',n.src=e,n.async=!0;var e=document.getElementsByTagName('script')[0];e.parentNode.insertBefore(n,e)}window.heap.serverConfig=SERVER_SIDE_CONFIG,window.heap.init(window.heap.envId,window.heap.clientConfig,window.heap.serverConfig),e()}();
+(function () {
+    // This object is created & validated on the generation service level
+    var SERVER_SIDE_CONFIG = {};
+
+    window.heap.serverConfig = SERVER_SIDE_CONFIG;
+
+    function getPluginKey(arr) {
+        if (arr.indexOf('core') === -1) {
+            arr.push('core');
+        }
+        var sortedArray = arr.sort(function (a, b) {
+            if (a < b) {
+                return -1;
+            } else if (a > b) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        return sortedArray.join('-');
+    }
+
+    function constructSDKEndpoint() {
+        // TODO - we can also support customer-inputted plugins, we would just find the intersection of the 2
+        var plugins = getPluginKey(SERVER_SIDE_CONFIG.sdk.plugins || []);
+        var clientSdkConfig = (heap && heap.clientConfig && heap.clientConfig.sdk) || {};
+        var domain = clientSdkConfig.domain || SERVER_SIDE_CONFIG.sdk.domain;
+        var version = clientSdkConfig.version || SERVER_SIDE_CONFIG.sdk.version;
+        var heapjsType =
+            clientSdkConfig.version || !SERVER_SIDE_CONFIG.sdk.isExperimentalVersion
+                ? 'heapjs-static'
+                : 'heapjs-experimental';
+
+        return domain + '/v5/' + heapjsType + '/' + version + '/' + plugins + '/heap.js';
+    }
+
+    function loadScript() {
+        var url = constructSDKEndpoint();
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+        script.async = true;
+        var firstScript = document.getElementsByTagName('script')[0];
+        firstScript.parentNode.insertBefore(script, firstScript);
+    }
+
+    window.heap.init(window.heap.envId, window.heap.clientConfig, window.heap.serverConfig);
+
+    loadScript();
+})();
